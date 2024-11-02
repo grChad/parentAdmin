3dd5ae1a013f850d57c5fc69255f7355bb38d8d8
grChad <bgos-jesus@hotmail.com>
2024-11-02 09:36:08 -0500

    chore: delete button SignOut

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,4 @@
 import { View, Text, Button } from 'react-native'
-import { SignOut } from '../../service/supabase'
 
 export default function HomeScreen() {
 	return (
@@ -7,7 +6,6 @@ export default function HomeScreen() {
 			<Text>Home Screen</Text>
 
 			<View style={{ marginVertical: 20 }} />
-			<Button title="Sign Out" onPress={SignOut} />
 		</View>
 	)
 }
