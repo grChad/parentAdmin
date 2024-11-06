a8f7bc24baaa1e1d1f35221b89ecc341c46dc15c
grChad <bgos-jesus@hotmail.com>
2024-11-05 20:05:17 -0500

    fixme: route to image

--- src/constants/list-courses.ts
+++ src/constants/list-courses.ts
@@ -24,59 +24,59 @@ export type ListCourseProps = {
 const imagesCourses: ListCourseProps[] = [
 	{
 		course: Courses.chemistry,
-		image: require('../../../assets/images/courses/chemistry.png'),
+		image: require('../../assets/images/courses/chemistry.png'),
 	},
 	{
 		course: Courses.maths,
-		image: require('../../../assets/images/courses/maths.png'),
+		image: require('../../assets/images/courses/maths.png'),
 	},
 	{
 		course: Courses.english,
-		image: require('../../../assets/images/courses/english.png'),
+		image: require('../../assets/images/courses/english.png'),
 	},
 	{
 		course: Courses.philosophy,
-		image: require('../../../assets/images/courses/philosopher.png'),
+		image: require('../../assets/images/courses/philosopher.png'),
 	},
 	{
 		course: Courses.religion,
-		image: require('../../../assets/images/courses/pray.png'),
+		image: require('../../assets/images/courses/pray.png'),
 	},
 	{
 		course: Courses.literature,
-		image: require('../../../assets/images/courses/shakespeare.png'),
+		image: require('../../assets/images/courses/shakespeare.png'),
 	},
 	{
 		course: Courses.history,
-		image: require('../../../assets/images/courses/evolution.png'),
+		image: require('../../assets/images/courses/evolution.png'),
 	},
 	{
 		course: Courses.biology,
-		image: require('../../../assets/images/courses/adn.png'),
+		image: require('../../assets/images/courses/adn.png'),
 	},
 	{
 		course: Courses.physics,
-		image: require('../../../assets/images/courses/atom.png'),
+		image: require('../../assets/images/courses/atom.png'),
 	},
 	{
 		course: Courses.geography,
-		image: require('../../../assets/images/courses/geography.png'),
+		image: require('../../assets/images/courses/geography.png'),
 	},
 	{
 		course: Courses.art,
-		image: require('../../../assets/images/courses/art.png'),
+		image: require('../../assets/images/courses/art.png'),
 	},
 	{
 		course: Courses.civic_and_etic,
-		image: require('../../../assets/images/courses/civic.png'),
+		image: require('../../assets/images/courses/civic.png'),
 	},
 	{
 		course: Courses.informatics,
-		image: require('../../../assets/images/courses/programing.png'),
+		image: require('../../assets/images/courses/programing.png'),
 	},
 	{
 		course: Courses.business,
-		image: require('../../../assets/images/courses/briefcase.png'),
+		image: require('../../assets/images/courses/briefcase.png'),
 	},
 ]
 
